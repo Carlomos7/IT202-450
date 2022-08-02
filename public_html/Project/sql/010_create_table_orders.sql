35acda13f5cf1cbbc2a34d6d07e0a80100f70b2b
Carlomos7 <cs525@njit.edu>
2022-08-02 18:37:49 -0400

    Added orders and orderproducts table. Completed purchase button. Started Checkout page

--- public_html/Project/sql/010_create_table_orders.sql
+++ public_html/Project/sql/010_create_table_orders.sql
@@ -1,10 +1,13 @@
 CREATE TABLE IF NOT EXISTS Orders(
     id int AUTO_INCREMENT PRIMARY KEY,
-    order_id int,
+    user_id int,
+    total_price int,
     user_address text,
-    payment_type text,
-    payment_value int,
+    payment_method text,
+    money_recieved int,
+    first_name text,
+    last_name text,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-    FOREIGN KEY (order_id) REFERENCES Orders(id)
+    FOREIGN KEY (user_id) REFERENCES Users(id)
 )
